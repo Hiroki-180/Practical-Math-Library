f8d94201a6c866f0b80f1914ed7f66aab9393f46
Hiroki-180 <hirokik012@gmail.com>
2018-03-30 04:55:17 +0900

    add numeric_simd files

---
+++ include/PML/Math/numeric_simd.h
@@ -0,0 +1,18 @@
+#ifndef MATH_NUMERIC_SIMD_H
+#define MATH_NUMERIC_SIMD_H
+
+#pragma once
+
+#include <PML/Core/NamespaceMacros.h>
+#include <PML/Core/aligned_vector.h>
+
+PML_MATH_NS_BEGIN
+
+namespace simd {
+
+
+}
+
+PML_MATH_NS_END
+
+#endif
---
+++ src/Math/numeric_simd.cpp
@@ -0,0 +1,5 @@
+#include <PML/Math/numeric_simd.h>
+
+PML_MATH_NS_BEGIN
+
+PML_MATH_NS_END
