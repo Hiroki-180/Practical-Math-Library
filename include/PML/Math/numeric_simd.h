88e517daf4074b31de89f27886acaead6d451b7b
Hiroki-180 <hirokik012@gmail.com>
2018-03-30 17:42:57 +0900

    add accumulate_ADX

--- include/PML/Math/numeric_simd.h
+++ include/PML/Math/numeric_simd.h
@@ -14,6 +14,10 @@ namespace simd {
 
     namespace detail {
 
+        double accumulate_aligned_AVX(
+            const double* inA,
+            std::size_t inSize);
+
         double inner_prod_aligned_AVX(
             const double* inA,
             const double* inB,
@@ -45,6 +49,28 @@ namespace simd {
         const std::vector<double>& inB)
     {
         return inner_prod_AVX(inA.data(), inB.data(), inA.size());
+    }
+
+
+    inline double accumulate_aligned_AVX(
+        const aligned_array<double>& inA,
+        std::size_t inSize)
+    {
+        return detail::accumulate_aligned_AVX(inA.get(), inSize);
+    }
+
+    inline double accumulate_aligned_AVX(const align32_vector<double>& inA)
+    {
+        return detail::accumulate_aligned_AVX(inA.data(), inA.size());
+    }
+
+    double accumulate_AVX(
+        const double* inA,
+        std::size_t inSize);
+
+    inline double accumulate_AVX(const std::vector<double>& inA)
+    {
+        return accumulate_AVX(inA.data(), inA.size());
     }
 }
 
--- src/Math/numeric_simd.cpp
+++ src/Math/numeric_simd.cpp
@@ -6,7 +6,46 @@ namespace simd {
 
     namespace detail {
 
-        double inner_prod_aligned_AVX(const double* inA, const double* inB, std::size_t inSize)
+        double accumulate_aligned_AVX(
+            const double* inA,
+            std::size_t inSize)
+        {
+            __m128d lSum128 = _mm_setzero_pd();
+
+            for (std::size_t i = 0;i < inSize;i += 4)
+            {
+                // lA256 = ( A[i],  A[i+1],  A[i+2],  A[i+3] )
+                __m256d lA256 = _mm256_load_pd(&inA[i]);
+
+                // hiDual = ( A[i], A[i+1] )
+                const __m128d hiDual = _mm256_extractf128_pd(lA256, 1);
+                // loDual = ( A[i+2], A[i+3] )
+                const __m128d loDual = _mm256_castpd256_pd128(lA256);
+
+                // sumQuad = ( A[i]+A[i+2], A[i+1]+A[i+3] )
+                const __m128d sumDual = _mm_add_pd(loDual, hiDual);
+
+                // lSum128 = ( A[0]+...+A[i]+A[i+2], A[1]+...+A[i+1]+A[i+3] );
+                lSum128 = _mm_add_pd(lSum128, sumDual);
+            }
+
+            PML_STATIC_ALLIGN(32) double lPartialSum[2] = { 0 };
+            _mm_store_pd(lPartialSum, lSum128);
+            auto lSum = lPartialSum[0] + lPartialSum[1];
+
+            const std::size_t lOffset = (inSize - inSize % 4);
+            for (std::size_t i = lOffset;i < inSize; ++i)
+            {
+                lSum += inA[i];
+            }
+
+            return lSum;
+        }
+
+        double inner_prod_aligned_AVX(
+            const double* inA,
+            const double* inB,
+            std::size_t inSize)
         {
             __m256d lSum256 = _mm256_setzero_pd();
 
@@ -32,7 +71,10 @@ namespace simd {
         }
     }
 
-    double inner_prod_AVX(const double* inA, const double* inB, std::size_t inSize)
+    double inner_prod_AVX(
+        const double* inA,
+        const double* inB,
+        std::size_t inSize)
     {
         __m256d lSum256 = _mm256_setzero_pd();
 
@@ -52,6 +94,42 @@ namespace simd {
         for (std::size_t i = lOffset;i < inSize; ++i)
         {
             lSum += inA[i] * inB[i];
+        }
+
+        return lSum;
+    }
+
+    double accumulate_AVX(
+        const double* inA,
+        std::size_t inSize)
+    {
+        __m128d lSum128 = _mm_setzero_pd();
+
+        for (std::size_t i = 0;i < inSize;i += 4)
+        {
+            // lA256 = ( A[i],  A[i+1],  A[i+2],  A[i+3] )
+            __m256d lA256 = _mm256_loadu_pd(&inA[i]);
+
+            // hiDual = ( A[i], A[i+1] )
+            const __m128d hiDual = _mm256_extractf128_pd(lA256, 1);
+            // loDual = ( A[i+2], A[i+3] )
+            const __m128d loDual = _mm256_castpd256_pd128(lA256);
+
+            // sumQuad = ( A[i]+A[i+2], A[i+1]+A[i+3] )
+            const __m128d sumDual = _mm_add_pd(loDual, hiDual);
+
+            // lSum128 = ( A[0]+...+A[i]+A[i+2], A[1]+...+A[i+1]+A[i+3] );
+            lSum128 = _mm_add_pd(lSum128, sumDual);
+        }
+
+        PML_STATIC_ALLIGN(32) double lPartialSum[2] = { 0 };
+        _mm_store_pd(lPartialSum, lSum128);
+        auto lSum = lPartialSum[0] + lPartialSum[1];
+
+        const std::size_t lOffset = (inSize - inSize % 4);
+        for (std::size_t i = lOffset;i < inSize; ++i)
+        {
+            lSum += inA[i];
         }
 
         return lSum;
