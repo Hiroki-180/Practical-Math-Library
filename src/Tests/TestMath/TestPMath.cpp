4514646af5a4c90e2c3977990c586c7420094bca
Hiroki-180 <hirokik012@gmail.com>
2018-09-22 17:21:33 +0900

    simplify TestPMath.cpp

--- src/Tests/TestMath/TestPMath.cpp
+++ src/Tests/TestMath/TestPMath.cpp
@@ -7,100 +7,25 @@
 #include <cmath>
 #include <functional>
 
-TEST(TestPMath, exp_value)
+class pmath : public ::testing::TestWithParam<
+    std::tuple<std::string, std::function<double(double)>, std::function<double(double)>>> {};
+
+TEST_P(pmath, value)
 {
-    for (auto i = 0; i < 200; ++i)
-    {
-        const auto lX = -5.0 + 0.05*i;
-        EXPECT_DOUBLE_EQ(std::exp(lX), pml::exp(lX));
-    }
-}
+    const auto& lSTLF = std::get<1>(GetParam());
+    const auto& lPMLF = std::get<2>(GetParam());
 
-TEST(TestPMath, exp_performance)
-{
-    const auto lTestNum
-#ifdef NDEBUG
-        = 3000000;
-#else
-        = 100000;
-#endif
-
-    const auto lStride = 10.0 / lTestNum;
-
-    auto lSum = 0.0;
-    const auto lStart = std::chrono::system_clock::now();
-    for (auto i = 0; i < lTestNum; ++i)
-    {
-        const auto lX = -5.0 + lStride * i;
-        lSum += std::exp(lX);
-    }
-    const auto lEnd = std::chrono::system_clock::now();
-    const auto lElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(lEnd - lStart).count();
-
-    auto lSumPML = 0.0;
-    const auto lStartPML = std::chrono::system_clock::now();
-    for (auto i = 0; i < lTestNum; ++i)
-    {
-        const auto lX = -5.0 + lStride * i;
-        lSumPML += pml::exp(lX);
-    }
-    const auto lEndPML = std::chrono::system_clock::now();
-    const auto lElapsedPML = std::chrono::duration_cast<std::chrono::milliseconds>(lEndPML - lStartPML).count();
-
-    EXPECT_DOUBLE_EQ(lSum, lSumPML);
-
-    std::cout
-#ifdef NDEBUG
-        << "---Release Mode---\n"
-#else
-        << "---Debud Mode---\n"
-#endif
-        << "STL:" << lElapsed << "[msec],\n"
-        << "PML:" << lElapsedPML << "[msec],\n"
-        << std::setprecision(3)
-        << "PML is " << (lElapsed / (double)(lElapsedPML)) << " faster than this compiler's STL implimentation.\n";
-
-#ifdef NDEBUG
-    EXPECT_LE(lElapsedPML, lElapsed);
-#endif
-}
-
-TEST(TestPMath, expv_value)
-{
-    std::vector<double> lExponent;
-
-    for (auto i = 0; i < 200; ++i)
-    {
-        lExponent.push_back(-5.0 + 0.05*i);
-    }
-
-    std::vector<double> lResult(lExponent.size());
-    pml::expv(lExponent.data(), lResult.data(), lExponent.size());
-
-    for (auto i = 0; i < 200; ++i)
-    {
-        EXPECT_DOUBLE_EQ(std::exp(lExponent[i]), lResult[i]);
-    }
-}
-
-
-class pmath_trigonometric : public ::testing::TestWithParam<
-    std::pair<std::function<double(double)>, std::function<double(double)>>> {};
-
-TEST_P(pmath_trigonometric, value)
-{
-    const auto& lSTLF = GetParam().first;
-    const auto& lPMLF = GetParam().second;
-
-    for (auto i = -1000000; i < 1000000; ++i)
+    for (auto i = -10000; i < 10000; ++i)
     {
         const auto lX = static_cast<double>(i*0.0001);
-        EXPECT_DOUBLE_EQ(lSTLF(lX), lPMLF(lX)) << lX;
+        EXPECT_DOUBLE_EQ(lSTLF(lX), lPMLF(lX)) << std::get<0>(GetParam()) << "(" << lX <<")\n";
     }
 }
 
-TEST_P(pmath_trigonometric, performance)
+TEST_P(pmath, performance)
 {
+    std::cout << "Test of " << std::get<0>(GetParam()) << " function.\n";
+
     const auto lTestNum
 #ifdef NDEBUG
         = 10000000;
@@ -109,7 +34,7 @@ TEST_P(pmath_trigonometric, performance)
 #endif
 
     auto lSum = 0.0;
-    const auto& lSTLF = GetParam().first;
+    const auto& lSTLF = std::get<1>(GetParam());
     const auto lStart = std::chrono::system_clock::now();
     for (auto i = 0; i < lTestNum; ++i)
     {
@@ -120,7 +45,7 @@ TEST_P(pmath_trigonometric, performance)
     const auto lElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(lEnd - lStart).count();
 
     auto lSumPML = 0.0;
-    const auto& lPMLF = GetParam().second;
+    const auto& lPMLF = std::get<2>(GetParam());
     const auto lStartPML = std::chrono::system_clock::now();
     for (auto i = 0; i < lTestNum; ++i)
     {
@@ -150,11 +75,17 @@ TEST_P(pmath_trigonometric, performance)
 }
 
 INSTANTIATE_TEST_CASE_P(
-    name, pmath_trigonometric,
+    name, pmath,
     ::testing::Values(
-        std::make_pair(
+        std::make_tuple(
+            "sin",
             std::bind((double(*)(double))&std::sin, std::placeholders::_1),
             std::bind((double(*)(double))&pml::sin, std::placeholders::_1)),
-        std::make_pair(
+        std::make_tuple(
+            "cos",
             std::bind((double(*)(double))&std::cos, std::placeholders::_1),
-            std::bind((double(*)(double))&pml::cos, std::placeholders::_1))));
+            std::bind((double(*)(double))&pml::cos, std::placeholders::_1)),
+        std::make_tuple(
+            "exp",
+            std::bind((double(*)(double))&std::exp, std::placeholders::_1),
+            std::bind((double(*)(double))&pml::exp, std::placeholders::_1))));
